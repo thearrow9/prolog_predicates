e7410393ddbb0934cfc68df77615c19fab195660
Tihomir Yanev <tisho91@gmail.com>
2013-11-02 14:44:26 +0200

    Added subset_/2 generator and unique_list/2

--- main.pl
+++ main.pl
@@ -16,6 +16,26 @@ gen_member([_ | Tail], X) :- gen_member(Tail, X).
 remove_member([Head | Tail], Head, Tail).
 remove_member([Head | Tail], X, [Head | Result]) :- remove_member(Tail, X, Result).
 
+%generator subset_/2
+
+subset_([], []).
+subset_([Head | Tail], [Head | Result]) :- subset_(Tail, Result).
+subset_([_ | Tail], Result) :- subset_(Tail, Result).
+
+%predicate unique_list/2
+
+u_iter([], R, R).
+u_iter([Head | Tail], List, X) :- leave_one(List, Head, Result), u_iter(Tail, Result, X).
+
+leave_one([], R, [R]) :- !.
+leave_one([Head | Tail], Head, Result) :- leave_one(Tail, Head, Result).
+leave_one([Head | Tail], Element, [Head | Result]) :- leave_one(Tail, Element, Result).
+
+unique_list(List, X) :- u_iter(List, List, X).
+
+
+
+%tests
 :- begin_tests(testing).
 
 %append_ []
@@ -32,6 +52,15 @@ test(append_, nondet) :- append_([a, c], [a, b], [a, c, a, b]).
 test(remove_member, nondet) :- remove_member([a], a, []).
 test(remove_member, fail) :- remove_member([a], r, []).
 test(remove_member, nondet) :- remove_member([1, 2, 3, 2, 3, 2], 2, [1, 3, 2, 3, 2]).
+
+%subset_
+test(subset_, fail) :- subset_([1, 2, 3, 4], [1, 5]).
+test(subset_, nondet) :- subset_([1, 2, 3, 4], []).
+test(subset_, nondet) :- subset_([1, 2, 3, 4], [1, 2]).
+
+%leave_one
+test(leave_one, nondet) :- leave_one([5, 3, 2, 1, 5], 5, [3, 2, 1, 5]).
+
 
 :- end_tests(testing).
 
